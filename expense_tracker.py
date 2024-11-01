28a9b00a4858dc2a66b3d79ef64d8d595d0cdd54
PJ Williams <pakavijwilliams@gmail.com>
2024-11-01 16:28:30 -0500

    Initial commit and project setup.

---
+++ expense_tracker.py
@@ -0,0 +1,26 @@
+def main():
+    
+    # Get user input for expense.
+    get_user_expense()
+    
+    # Write expense to file.
+    save_expense_to_file()
+    
+    # Read file and summarize expense.
+    summarize_expenses()
+
+
+def get_user_expense():
+    print("Get user expense.")
+
+
+def save_expense_to_file():
+    print("Saving user expense.")
+
+
+def summarize_expenses():
+    print("Summarizing user expense.")
+
+
+if __name__ == "__main__":
+    main()
